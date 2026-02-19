0962e0ff53538b5def0d8a8eb95aed8d17716e16
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-19 12:06:44 +0600

    to do api data conditional rendering

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ btnCard.addEventListener("click", () => {
         .then(data => {
             showCard(data)
         })
+        .catch((error) => {
+            console.log("Error Message:", error);
+        })
 })
 // function for forEach;
 const showCard = (cards) => {
@@ -27,7 +30,42 @@ const showCard = (cards) => {
         </div>
         `
         cardsContainer.appendChild(div)
-        
+
     })
 }
 
+
+// To do api code start here;
+const todoFunk = () => {
+    // console.log("to do funk");
+    fetch("https://jsonplaceholder.typicode.com/todos")
+        .then(res => res.json())
+        .then(data => {
+            displayToDo(data);
+        })
+        .catch((error) => {
+            console.log("Error Message:", error);
+        })
+}
+// to do show ui funk;
+const displayToDo = (lists) => {
+    // console.log(lists);
+    const toDoContainer = document.getElementById("toDoContainer");
+    toDoContainer.innerHTML = '';
+    lists.forEach(list => {
+        // console.log(list);
+        const div = document.createElement("div");
+        div.innerHTML = `
+       
+        <div class="border-2 border-gray-500 mb-4 p-4 round-l ">
+            <div class = "flex gap-4">
+                 <p>${list.completed == true ? `<i class="fa-solid fa-circle-check"></i>` : `<i class="fa-solid fa-check"></i>`}</p>
+                 <h2 class="font-extrabold">${list.title}</h2>
+            </div>
+        
+        </div>
+        `
+        toDoContainer.append(div)
+    })
+}
+todoFunk()
