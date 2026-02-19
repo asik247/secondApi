d322cdc4140bf572aa30abad28f0a960b9080158
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-19 11:11:59 +0600

    Card Showing Dynamicly done

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 let btnCard = document.getElementById("btnCard");
-btnCard.addEventListener("click",()=>{
-    
+btnCard.addEventListener("click", () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            showCard(data)
+        })
 })
+// function for forEach;
+const showCard = (cards) => {
+    let cardsContainer = document.getElementById("cardsContainer");
+    cardsContainer.innerHTML = "";
+    cards.forEach((card) => {
+        // console.log(card);
+        let div = document.createElement("div");
+        div.innerHTML = `
+            <div class="card bg-primary text-primary-content w-full h-full">
+            <div class="card-body">
+                <h1>${card.id}</h1>
+                <h2 class="card-title">${card.title}</h2>
+                <p>${card.body}</p>
+                <div class="card-actions justify-end">
+                    <button class="btn">Buy Now</button>
+                </div>
+            </div>
+        </div>
+        `
+        cardsContainer.appendChild(div)
+        
+    })
+}
+
